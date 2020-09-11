9ee4aaa64fefa16b99dd83f6ded8fb7fc720cc1f
Mort094 <mort094@hotmail.com>
2020-09-11 10:55:45 +0200

    Add project files.

---
+++ TCPBogServer/Bogserver.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace TCPBogServer
+{
+    internal class Bogserver
+    {
+        internal void Start()
+        {
+           TcpListener server = new TcpListener(IPAddress.Loopback, 4646);
+           server.Start();
+           while (true)
+           {
+               TcpClient socket = server.AcceptTcpClient();
+               Task.Run(() =>
+               {
+                   TcpClient tempSocket = socket;
+                   DoClient(tempSocket);
+               });
+           }
+        }
+
+        private void DoClient(TcpClient socket)
+        {
+            NetworkStream ns = socket.GetStream();
+
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+
+            while (true)
+            {
+                String str = sr.ReadLine();
+                String[] vs = str.Split(" ");
+
+                String strRetur = ("Resultat = ");
+                String test = vs[0];
+                if (test == "HentAlle")
+                {
+                    sw.WriteLine(strRetur + "virker ikke lige pt");
+                }
+            }
+        }
+    }
+}
---
+++ TCPBogServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TCPBogServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Bogserver worker = new Bogserver();
+          worker.Start();
+
+          Console.ReadLine();
+        }
+    }
+}
